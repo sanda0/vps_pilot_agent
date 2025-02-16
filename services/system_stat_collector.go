bbad214db72027e349c4074040c12f9ec01f1edf
sandakelu-dev <sandakelum.dev@gmail.com>
2025-02-16 23:22:35 +0530

    add network statistics to SystemStat and update collection logic

--- services/system_stat_collector.go
+++ services/system_stat_collector.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/sanda0/vps_pilot_agent/dto"
 	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/v3/net"
 	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
 )
@@ -15,14 +16,14 @@ import (
 func StartCollectSystemStat(ctx context.Context, msgChan chan dto.Msg, interval int) {
 	ticker := time.NewTicker(time.Duration(interval) * time.Second)
 	defer ticker.Stop()
-
+	var prevNetStats []net.IOCountersStat
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Stopping system stat collection")
 			return
 		case <-ticker.C:
-			err := collectStat(msgChan)
+			err := collectStat(msgChan, &prevNetStats, interval)
 			if err != nil {
 				fmt.Println("Error collecting system stat:", err)
 			}
@@ -31,7 +32,7 @@ func StartCollectSystemStat(ctx context.Context, msgChan chan dto.Msg, interval 
 
 }
 
-func collectStat(msgChan chan dto.Msg) error {
+func collectStat(msgChan chan dto.Msg, prevNetStats *[]net.IOCountersStat, interval int) error {
 	cpuUsage, err := cpu.Percent(time.Second, true)
 	if err != nil {
 		return err
@@ -42,10 +43,28 @@ func collectStat(msgChan chan dto.Msg) error {
 	}
 	diskUsage := 0.0
 
+	// Get current network statistics
+	netStats, err := net.IOCounters(false) // false: get total stats
+	if err != nil {
+		return err
+	}
+
+	var netSentPS, netRecvPS uint64
+	if len(*prevNetStats) > 0 {
+		// Calculate bytes per second
+		netSentPS = (netStats[0].BytesSent - (*prevNetStats)[0].BytesSent) / uint64(interval)
+		netRecvPS = (netStats[0].BytesRecv - (*prevNetStats)[0].BytesRecv) / uint64(interval)
+	}
+
+	// Store current stats for the next iteration
+	*prevNetStats = netStats
+
 	systemStat := dto.SystemStat{
 		CPUUsage:  cpuUsage,
 		MemUsage:  memUsage.UsedPercent,
 		DiskUsage: diskUsage,
+		NetSentPS: netSentPS,
+		NetRecvPS: netRecvPS,
 	}
 
 	systemStatJSON, err := json.Marshal(systemStat)
